654060f9e42cfb0064c163287aa04e97c80f3008
shayan <rasoolishayan18@gmail.com>
2024-07-23 22:53:55 +0330

    creating signin api

---
+++ pages/api/Auth/signin.js
@@ -0,0 +1,36 @@
+import User from "../../../Model/User";
+
+export default async function handler(req , res) {
+    if (req.method === "POST") {
+        return;
+    }
+
+    try {
+        await  ConnectedDB()
+    } catch (error) {
+        return res.status(500).json({status : "failed" , message : "Error in connecting to dataBase"})
+    }
+
+    const {email , password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).json({status : "failed" , message : "Invalid Data"})
+    }
+
+    const  user = User.findOne({email : email})
+
+    if(!user){
+        return res.status(404).json({status : "failed" , message : "user doesnt exist"})
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
