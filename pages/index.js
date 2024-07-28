69ddbff5ebb9b7dfaaeb8745083ec7ce5a61baa7
shayan <rasoolishayan18@gmail.com>
2024-07-28 13:59:44 +0330

    showing elements base on the auth

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,41 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoggedIn , setIsLoggedIn] = useState(false)
+  
+  useEffect(() => {
+    fetch("/api/auth")
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status === "success") setIsLoggedIn(true)
+    })
+
+  } , [])
+
   const signOutHandler = async () => {
     const res = await fetch("api/Auth/signout");
     const data = res.json()
-    console.log(data);
+    if (data.status === "success") setIsLoggedIn(false)
+
 
   }
   return (
     <div>
-     <button><Link href='/dashboard'>Dashboard</Link></button>
-     <button><Link href='/signup'>Sign Up</Link></button>
-     <button><Link href='/signin'>Sign In</Link></button>
-     <button onClick={signOutHandler}>Log out</button>
+      {isLoggedIn ? (
+        <>
+        <button><Link href='/dashboard'>Dashboard</Link></button>
+        <button onClick={signOutHandler}>Log out</button>
+        </>
+      ) : null}
+ 
+    {!isLoggedIn ? (
+      <>
+      <button><Link href='/signup'>Sign Up</Link></button>
+      <button><Link href='/signin'>Sign In</Link></button>
+      </>
+
+    ): null}
     </div>
   )
 }
